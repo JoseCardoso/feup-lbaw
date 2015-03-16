e97a2b5e68396e16ba839ed562118167c8a43d86
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-03-16 02:56:05 +0000

    working on explore page

--- web/templates/explore.php
+++ web/templates/explore.php
@@ -3,7 +3,63 @@
 	<?php include 'navbar.php'; ?>
 	
 	<div class="row">
-		<div class="small-12 columns">
-			<h1>Explore</h1>
+		<div class="small-6 columns">
+			<div class="panel">
+				<div class="row">
+					<div class="small-6 columns">
+						<div class="row">
+							<div class="small-4 columns">
+								<p class="text-center">50</p>
+								<p class="text-center">votes</p>
+							</div>
+							<div class="small-4 columns">
+								<p class="text-center">4</p>
+								<p class="text-center">answers</p>
+							</div>
+							<div class="small-4 columns">
+								<p class="text-center">144</p>
+								<p class="text-center">views</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="small-6 columns">
+						<p>A Dona Beatriz vende EcoBooks?</p>
+						<p class="text-right"><a href="#">@jas</a></p>
+						<p>00:15 - 15 Jan 2015</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-6 columns">
+			<div class="panel">
+				<div class="row">
+					<div class="small-6 columns">
+						<div class="row">
+							<div class="small-4 columns">
+								<p class="text-center">50</p>
+								<p class="text-center">votes</p>
+							</div>
+							<div class="small-4 columns">
+								<p class="text-center">4</p>
+								<p class="text-center">answers</p>
+							</div>
+							<div class="small-4 columns">
+								<p class="text-center">144</p>
+								<p class="text-center">views</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="small-6 columns">
+						<p>Qual é a cor da FEUP?</p>
+						<p class="text-right"><a href="#">@augustosousa</a></p>
+						<p>00:12 - 15 Jan 2015</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
