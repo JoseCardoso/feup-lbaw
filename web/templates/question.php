cfa305fefa12905e7af11a2bfc3add72be02489c
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-03-23 01:39:38 +0000

    added comments to question and answers

--- web/templates/question.php
+++ web/templates/question.php
@@ -1,6 +1,6 @@
 <div class="question-modal-content favorite">
 	<div class="row">
-		<div class="small-2 columns">
+		<div class="small-4 large-2 columns">
 			<div class="row">
 				<div class="small-12 columns text-center">
 					<i class="fi-like"></i>
@@ -25,17 +25,23 @@
 				</div>
 			</div>
 		</div>
-		<div class="small-10 columns">
+
+		<div class="small-8 large-10 columns">
 			<p class="question-text">Que tipo de gomas são vendidas na D. Beatriz?</p>
 			<p class="text-right question-author"><a href="#">@ferrolho</a></p>
 			<p class="text-right question-date">00h15 - 15 Jan 2015</p>
+		</div>
+
+		<div class="small-10 columns comments">
+			<hr>
+			<p>também gostava de saber <a href="#">@zoid</a></p>
 		</div>
 	</div>
 
 	<hr>
 
 	<div class="row upvote checked-answer">
-		<div class="small-2 columns">
+		<div class="small-4 large-2 columns">
 			<div class="row">
 				<div class="small-12 columns text-center">
 					<i class="fi-like"></i>
@@ -60,17 +66,25 @@
 				</div>
 			</div>
 		</div>
-		<div class="small-10 columns">
+
+		<div class="small-8 large-10 columns">
 			<p>Marshmallows e aquelas gomas ácidas.</p>
 			<p class="text-right question-author"><a href="#">@botas</a></p>
 			<p class="text-right question-date">00h30 - 15 Jan 2015</p>
+		</div>
+
+		<div class="small-10 columns comments">
+			<hr>
+			<p>exactamente :) <a href="#">@mariamir</a></p>
+			<hr class="dashed">
+			<p>nem mais <a href="#">@ferrolho</a></p>
 		</div>
 	</div>
 
 	<hr class="dashed">
 
 	<div class="row downvote">
-		<div class="small-2 columns">
+		<div class="small-4 large-2 columns">
 			<div class="row">
 				<div class="small-12 columns text-center">
 					<i class="fi-like"></i>
@@ -89,10 +103,20 @@
 				</div>
 			</div>
 		</div>
-		<div class="small-10 columns">
-			<p>Chocolates, marshmallows e finnibons.</p>
+
+		<div class="small-8 large-10 columns">
+			<p>A Dona Beatriz não vende gomas.</p>
 			<p class="text-right question-author"><a href="#">@vecton</a></p>
 			<p class="text-right question-date">00h36 - 15 Jan 2015</p>
+		</div>
+
+		<div class="small-10 columns comments">
+			<hr>
+			<p>LOL! Vende sim... <a href="#">@botas</a></p>
+			<hr class="dashed">
+			<p>^ isto. Estão logo à direita da entrada, a seguir aos frigoríficos. <a href="#">@mariamir</a></p>
+			<hr class="dashed">
+			<p>rime <a href="#">@ferrolho</a></p>
 		</div>
 	</div>
 </div>
