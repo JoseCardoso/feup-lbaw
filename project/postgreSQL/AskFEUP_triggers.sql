f9dcda2fd1de87fb2ac9a8676ea348ac930516a6
JoseCardoso <josecardos@gmail.com>
2015-04-12 18:20:55 +0100

    more triggers

--- project/postgreSQL/AskFEUP_triggers.sql
+++ project/postgreSQL/AskFEUP_triggers.sql
@@ -12,3 +12,43 @@ CREATE TRIGGER rightAnswer INSTEAD OF UPDATE OF correcta
 		) > 0 ) );
 	BEGIN
 	END;
+
+/*CREATE TRIGGER GeneratePack
+AFTER INSERT ON RecomendacaoInstrumentoAcessorio
+WHEN(NEW.prioridade = 10)
+BEGIN
+INSERT INTO Pack(nome,preco) VALUES ('Auto Pack' || (SELECT 1+MAX(idPack) FROM Pack) ,0);
+INSERT INTO ArtigoPack(idArtigo,idPack) VALUES (NEW.idArtigo1,(SELECT MAX(idPack) FROM Pack));
+INSERT INTO ArtigoPack(idArtigo,idPack) VALUES (NEW.idArtigo2,(SELECT MAX(idPack) FROM Pack));
+END;
+*/
+
+
+
+CREATE TRIGGER DeleteMembro
+AFTER DELETE ON Utilizador
+BEGIN
+DELETE FROM Membro
+	WHERE Utilizador.utilizadorID = Membro.membroID;
+END;
+
+CREATE TRIGGER DeleteContribuicaoFromMembro
+AFTER DELETE ON Membro
+BEGIN
+DELETE FROM Contribuicao
+	WHERE Contribuicao.membroID = Membro.membroID;
+END;
+
+CREATE TRIGGER DeleteContribuicao
+AFTER DELETE ON Contribuicao
+BEGIN
+DELETE FROM Comentario
+	WHERE Contribuicao.contribuicaoID = Comentario.comentarioID;
+DELETE FROM Pergunta
+	WHERE Contribuicao.contribuicaoID = Pergunta.perguntaID;
+DELETE FROM Resposta
+	WHERE Contribuicao.contribuicaoID = Resposta.respostaID;
+END;
+
+
+
