04f596e5222f01173f9ea4925d210f306e0dc913
botas222 <pereiraffjoao1993@gmail.com>
2015-04-12 05:08:23 +0100

    minor changes on queries v3

--- project/postgreSQL/AskFEUP_queries.sql
+++ project/postgreSQL/AskFEUP_queries.sql
@@ -25,26 +25,20 @@ CREATE VIEW IF NOT EXISTS comentariosAContribuiçao AS
 SELECT Comentário.comentarioID FROM Contribuição , Comentário
 WHERE Comentário.contribuiçãoID = Comentário.comentarioID;
 
-/** 10. Lista de utilizador que possuem um determinado Badge **/
-DROP VIEW IF EXISTS membroPorBadge;
-CREATE VIEW IF NOT EXISTS membroPorBadge AS
-SELECT Membro.membroID FROM Membro , Badge, JoinBadgeToMembro
-WHERE Membro.membroID = JoinBadgeToMembro.membroID AND Badge.badgeID = JoinBadgeToMembro.badgeID;
+/** 10. Lista de utilizadores que possuem um determinado Badge (vice-versa) (testado) **/
+SELECT "Badge".nome AS "Nome", "Membro"."primeiroNome" AS "Membro"
+	FROM "Membro", "Badge", "BadgeMembro"
+	WHERE "Membro"."membroID" = "BadgeMembro"."membroID"
+	AND "Badge"."badgeID" = "BadgeMembro"."badgeID";
 
-/** 11. Lista de badges que pertencem a determinado utilizador **/
-DROP VIEW IF EXISTS badgePorMembro;
-CREATE VIEW IF NOT EXISTS badgePorMembro AS
-SELECT Badge.badgeID FROM Membro , Badge, JoinBadgeToMembro
-WHERE Membro.membroID = JoinBadgeToMembro.membroID AND Badge.badgeID = JoinBadgeToMembro.badgeID;
-
-/** 12. Lista de perguntas ordenadas por data de criação (recentes) **/
+/** 11. Lista de perguntas ordenadas por data de criação (recentes) **/
 DROP VIEW IF EXISTS PerguntasOrdenadasPorAno;
 CREATE VIEW IF NOT EXISTS PerguntasOrdenadasPorAno AS
 SELECT Pergunta.perguntaID FROM Pergunta, Contribuição
 WHERE Pergunta.perguntaID = Contribuição.contribuiçãoID
 ORDER BY Contribuição.data;
 
-/** 13. Lista de perguntas mais populares (votos) **/
+/** 12. Lista de perguntas mais populares (votos) **/
 DROP VIEW IF EXISTS PerguntasPopulares;
 CREATE VIEW IF NOT EXISTS PerguntasPopulares AS
 SELECT Pergunta.perguntaID FROM Pergunta, Contribuição
