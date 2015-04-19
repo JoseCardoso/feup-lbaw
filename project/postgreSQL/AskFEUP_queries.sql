bbba1ef57a38062810abd1d1b97d9085cb3d898a
botas222 <pereiraffjoao1993@gmail.com>
2015-04-19 23:09:30 +0100

    a7 and a8 finally are been reviewed

--- project/postgreSQL/AskFEUP_queries.sql
+++ project/postgreSQL/AskFEUP_queries.sql
@@ -1,9 +1,6 @@
 /** AskFEUP **/
 /** Queries **/
 
-/**.print \n
-.width 8 30 **/
-
 /** 1. Lista de perguntas ordenadas por pontuação (Testado) **/
 create view askfeup.perguntasporpontuacao as
 SELECT pergunta.perguntaid AS id, pergunta.texto AS conteudo, membro.primeironome AS membro, contribuicao.diferencavotos AS pontuacao
@@ -11,13 +8,46 @@ SELECT pergunta.perguntaid AS id, pergunta.texto AS conteudo, membro.primeironom
   WHERE pergunta.perguntaid = contribuicao.contribuicaoid AND contribuicao.membroid = membro.membroid
   ORDER BY contribuicao.diferencavotos DESC;
 
- /** 2. Lista de perguntas por utilizador (em que tenha actividade e não só as próprias) **/
+/** 2. Lista de perguntas por utilizador (em que tenha actividade e não só as próprias) **/
 create view askfeup.perguntasporutilizador as
 SELECT utilizador.username AS "user", pergunta.perguntaid AS id, pergunta.texto AS conteudo
    FROM utilizador, pergunta, membro, comentario, resposta, contribuicao
   WHERE resposta.perguntaid = pergunta.perguntaid AND utilizador.utilizadorid = contribuicao.membroid AND pergunta.perguntaid = contribuicao.contribuicaoid
   ORDER BY utilizador.username;
 
+/** 3. Lista de Pergunta Favoritas por Utilizador **/
+create view askfeup.favoritasporutilizador as
+ SELECT membro.ultimonome, membro.primeironome, pergunta.texto, pergunta.descricao
+   FROM membro
+   JOIN favorita ON membro.membroid = favorita.membroid
+   JOIN pergunta ON favorita.perguntaid = pergunta.perguntaid;
+
+/** 4. Lista de Perguntas por Categoria **/
+create view askfeup.perguntasporcategoria as
+SELECT pergunta.perguntaid, pergunta.categoriaid, pergunta.texto, pergunta.descricao, categoria.tipo
+   FROM pergunta
+   JOIN categoria ON pergunta.categoriaid = categoria.categoriaid;
+
+/** 5. Lista de Perguntas por Tags**/
+CREATE VIEW askfeup.PerguntasPorTags AS
+SELECT tag.nome, pergunta.texto
+   FROM perguntatag
+   JOIN tag ON perguntatag.tagid = tag.tagid
+   JOIN pergunta ON perguntatag.perguntaid = pergunta.perguntaid;
+
+/** 6. Lista de Respostas assinaladas como Correctas**/
+create view askfeup.respostascorrectas as
+SELECT pergunta.texto, resposta.correcta, resposta.descricao
+   FROM pergunta
+   JOIN resposta ON pergunta.perguntaid = resposta.perguntaid
+  WHERE resposta.correcta = true;
+
+/** 7. Lista de Respostas dadas por um membro**/
+CREATE VIEW askfeup.RespostasPorMembro AS
+SELECT resposta.correcta, resposta.descricao
+   FROM membro
+   JOIN contribuicao ON membro.membroid = contribuicao.membroid
+   JOIN resposta ON contribuicao.contribuicaoid = resposta.respostaid;
 
 /** 8. Lista de respostas a uma pergunta **/
 create view askfeup.respostasapergunta as
@@ -37,6 +67,13 @@ SELECT badge.nome, membro.primeironome AS membro
    FROM membro, badge, badgemembro
   WHERE membro.membroid = badgemembro.membroid AND badge.badgeid = badgemembro.badgeid;
 
+/** 11. Lista de perguntas ordenadas por data de criação **/
+CREATE VIEW askfeup.PerguntasPorDataDeCriacao AS
+ SELECT pergunta.perguntaid, pergunta.texto
+   FROM pergunta, contribuicao
+  WHERE pergunta.perguntaid = contribuicao.contribuicaoid
+  ORDER BY contribuicao.data;
+
 /** 12. Lista de perguntas mais populares (votos) **/
 create view askfeup.perguntaspopulares as
 SELECT pergunta.perguntaid
@@ -44,41 +81,7 @@ SELECT pergunta.perguntaid
   WHERE pergunta.perguntaid = contribuicao.contribuicaoid
   ORDER BY contribuicao.votospositivos + contribuicao.votosnegativos DESC;
 
-/** Lista de Pergunta Favoritas por Utilizador **/
-create view askfeup.favoritasporutilizador as
- SELECT membro.ultimonome, membro.primeironome, pergunta.texto, pergunta.descricao
-   FROM membro
-   JOIN favorita ON membro.membroid = favorita.membroid
-   JOIN pergunta ON favorita.perguntaid = pergunta.perguntaid;
-
-/** Lista de Perguntas por Categoria **/
-create view askfeup.perguntasporcategoria as
-SELECT pergunta.perguntaid, pergunta.categoriaid, pergunta.texto, pergunta.descricao, categoria.tipo
-   FROM pergunta
-   JOIN categoria ON pergunta.categoriaid = categoria.categoriaid;
-
-/** Lista de Respostas assinaladas como Correctas**/
-create view askfeup.respostascorrectas as
-SELECT pergunta.texto, resposta.correcta, resposta.descricao
-   FROM pergunta
-   JOIN resposta ON pergunta.perguntaid = resposta.perguntaid
-  WHERE resposta.correcta = true;
-
-/** Lista de Perguntas por Tags**/
-CREATE VIEW askfeup.PerguntasPorTags AS
-SELECT tag.nome, pergunta.texto
-   FROM perguntatag
-   JOIN tag ON perguntatag.tagid = tag.tagid
-   JOIN pergunta ON perguntatag.perguntaid = pergunta.perguntaid;
-
-/** Lista de REspostas dadas por um Membro**/
-CREATE VIEW askfeup.RespostasPorMembro AS
-SELECT resposta.correcta, resposta.descricao
-   FROM membro
-   JOIN contribuicao ON membro.membroid = contribuicao.membroid
-   JOIN resposta ON contribuicao.contribuicaoid = resposta.respostaid;
-
-/** Lista de perguntas mais vistas **/
+/** 13. Lista de perguntas mais vistas **/
 CREATE VIEW askfeup.PerguntasMaisVistas AS
 SELECT pergunta.perguntaid, pergunta.texto, subquery1.views
    FROM pergunta, contribuicao, ( SELECT visualizacao.perguntaid, count(*) AS views
@@ -86,10 +89,4 @@ SELECT pergunta.perguntaid, pergunta.texto, subquery1.views
           GROUP BY visualizacao.perguntaid) subquery1
   WHERE pergunta.perguntaid = contribuicao.contribuicaoid AND subquery1.perguntaid = pergunta.perguntaid
   ORDER BY subquery1.views DESC;
-
-/** Lista de perguntas ordenadas por data de criação **/
-CREATE VIEW askfeup.PerguntasPorDataDeCriacao AS
- SELECT pergunta.perguntaid, pergunta.texto
-   FROM pergunta, contribuicao
-  WHERE pergunta.perguntaid = contribuicao.contribuicaoid
-  ORDER BY contribuicao.data;
+  
--- project/postgreSQL/AskFEUP_triggers.sql
+++ project/postgreSQL/AskFEUP_triggers.sql
@@ -1,9 +1,6 @@
 /** AskFEUP **/
 /** Triggers **/
 
-.headers ON
-.mode columns
-
 /** ID Independente entre Contribuição e Pergunta/Resposta **/
 CREATE FUNCTION askfeup.perguntaRespostaDiferenteID() RETURNS trigger AS $perguntaRespostaDiferenteID$
   begin
@@ -30,7 +27,7 @@ FOR EACH ROW EXECUTE PROCEDURE askfeup.perguntaRespostaDiferenteID();
 
 
 /** Um utilizador não poder votar na sua contribuição (Pergunta/Resposta) **/
-CREATE FUNCTION askfeup.notSelfVote() RETURNS trigger AS $notSelfVote$
+/*CREATE FUNCTION askfeup.notSelfVote() RETURNS trigger AS $notSelfVote$
   begin
     IF NEW.voteid IS NOT NULL THEN 
       new.utilizadorid AND new.contribuicaoid NOT IN (SELECT contribuicao.utilizadorid from contribuicao WHERE new.utilizadorid = contribuicao.utilizadorid AND new.contribuicaoid = contribuicao.contribuicaoid);
@@ -41,7 +38,7 @@ $notSelfVote$ LANGUAGE plpgsql;
 
 CREATE TRIGGER notSelfVote 
 BEFORE INSERT OR UPDATE ON askfeup.voto
-FOR EACH ROW EXECUTE PROCEDURE askfeup.notSelfVote();
+FOR EACH ROW EXECUTE PROCEDURE askfeup.notSelfVote();*/
 
 /** Actualizar votos**/
 CREATE FUNCTION askfeup.incrementVotes() RETURNS trigger AS $incrementVotes$
