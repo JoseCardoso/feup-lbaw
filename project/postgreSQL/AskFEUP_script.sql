9641b6015bdbee33d36a00cc2f05bee5d5d54a1f
botas222 <pereiraffjoao1993@gmail.com>
2015-04-23 01:14:51 +0100

    Errors

--- project/postgreSQL/AskFEUP_script.sql
+++ project/postgreSQL/AskFEUP_script.sql
@@ -310,21 +310,6 @@ CLUSTER askfeup.tag USING pk_tag;
 
 CLUSTER askfeup.categoria USING pk_categoria;
 
-<<<<<<< HEAD
-
-
-
-=======
-
-
-
->>>>>>> parent of f04e9d2... 2nd review of a7 and a8 almost done; time to gym
-
-
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 /** Actualizar votos**/
 CREATE FUNCTION askfeup.incrementVotes() RETURNS trigger AS $incrementVotes$
   BEGIN
@@ -474,10 +459,6 @@ SELECT pergunta.perguntaid, pergunta.texto, subquery1.views
           GROUP BY visualizacao.perguntaid) subquery1
   WHERE pergunta.perguntaid = contribuicao.contribuicaoid AND subquery1.perguntaid = pergunta.perguntaid
   ORDER BY subquery1.views DESC;
-=======
->>>>>>> parent of f04e9d2... 2nd review of a7 and a8 almost done; time to gym
-=======
->>>>>>> parent of f04e9d2... 2nd review of a7 and a8 almost done; time to gym
 
   /** AskFEUP **/
 /** Insert Data **/
@@ -1358,8 +1339,4 @@ SELECT pergunta.perguntaid, pergunta.texto, subquery1.views
            FROM askfeup.visualizacao
           GROUP BY visualizacao.perguntaid) subquery1
   WHERE pergunta.perguntaid = contribuicao.contribuicaoid AND subquery1.perguntaid = pergunta.perguntaid
-<<<<<<< HEAD
   ORDER BY subquery1.views DESC;
-=======
-  ORDER BY subquery1.views DESC;
->>>>>>> parent of f04e9d2... 2nd review of a7 and a8 almost done; time to gym
