b5fddc75e2e63bfdef9045fa37a9214421f46dc9
botas222 <pereiraffjoao1993@gmail.com>
2015-04-26 16:45:53 +0100

    error on connection

--- frmk/database/loadCities.php
+++ frmk/database/loadCities.php
@@ -6,10 +6,10 @@ function loadCities() {
 	$stmt->execute();
 	return $stmt->fetchAll();
 }
+
 function loadZIPByCity() {
     global $connection;
     $stmt = $connection->prepare("SELECT codigopostal FROM cidade WHERE nome = ? GROUP BY nome ORDER BY nome");
     $stmt->execute();
     return $stmt->fetchAll();
 }
-?>
