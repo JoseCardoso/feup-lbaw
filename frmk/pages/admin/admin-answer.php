0403e3c69f8003f163764721658d0f9ee21bd5a5
Jose Cardoso <josecardos@gmail.com>
2015-06-08 17:47:56 +0100

    added answer.php

---
+++ frmk/pages/admin/admin-answer.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once('../../config/config.php');
+
+include($BASE_DB . 'answer.php');
+
+if (isset($_GET['answer'])) {
+    try {
+        $answer = Answer::find($_GET['id']);
+
+        $smarty->assign('answer', $answer);
+        $smarty->assign('BASE_URL', $BASE_URL);
+
+        $smarty->display($BASE_TEMPLATES . 'admin/admin-answer.tpl');
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+} else
+    go('../../pages/admin/admin-answers.php');
